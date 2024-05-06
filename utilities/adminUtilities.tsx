554abd3a1fa91d0bf10bbfe1806bff502be3d731
Qinfeng Wu <qinfengwu90@gmail.com>
2024-05-05 20:12:20 -0600

    deleted old functions

--- utilities/adminUtilities.tsx
+++ utilities/adminUtilities.tsx
@@ -6,30 +6,6 @@ import jwt from 'jsonwebtoken';
 
 const JWT_SECRET_KEY = process.env.NEXT_PUBLIC_JWT_SECRET_KEY!;
 
-// TODO: delete this function
-export const adminLoginOld = (credential: { email: string; password: string }) => {
-  const loginUrl = `${SERVER_ORIGIN}/admins/login`;
-
-  // call the api to login
-  return fetch(loginUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(credential),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw Error("Fail to log in");
-      }
-      localStorage.setItem("adminEmail", credential.email);
-      return response.json();
-    })
-    .then((token: { token: string }) => {
-      localStorage.setItem("authToken", token.token);
-    });
-};
-
 export const adminLogin = async (credential: { email: string; password: string }) => {
   return getAdminInfoByEmail(credential.email)
     .then((data) => {
@@ -148,20 +124,6 @@ export async function getAllTickets(): Promise<{ tickets: Ticket[] }> {
   return { tickets: tickets };
 }
 
-export async function getAllTicketsOld(): Promise<{ tickets: Ticket[] }> {
-  const url = `${SERVER_ORIGIN}/admins/all-tickets`;
-
-  return fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-    },
-  }).then((response) => {
-    handleResponseStatus(response, "Fail to get tickets");
-    return response.json();
-  });
-}
-
 export const updateTicketStatus = async (
   ticketId: number,
   status: string,
@@ -176,25 +138,3 @@ export const updateTicketStatus = async (
     throw new Error("Fail to update ticket status");
   }
 }
-
-export const updateTicketStatusOld = (
-  ticketId: number,
-  status: string,
-  adminResponse: string,
-) => {
-  const url = `${SERVER_ORIGIN}/admins/update-ticket-status`;
-  return fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-    },
-    body: JSON.stringify({
-      ticketId: ticketId,
-      status: status,
-      adminResponse: adminResponse,
-    }),
-  }).then((response) => {
-    handleResponseStatus(response, "Fail to update ticket status");
-  });
-};
--- utilities/generalUtilities.ts
+++ utilities/generalUtilities.ts
@@ -22,20 +22,6 @@ export const deleteTicket = async (ticketId: number) => {
     .eq("id", ticketId);
 }
 
-// TODO: delete this function
-export const deleteTicketOld = async (ticketId: number) => {
-  const url = `${SERVER_ORIGIN}/delete-ticket`;
-
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ticketId: ticketId}),
-  });
-  handleResponseStatus(response, "Fail to delete ticket");
-};
-
 export const formatTicketStatus = (status: string) => {
   let newStatus = status.charAt(0).toUpperCase() + status.slice(1);
   return newStatus.replace(/_/g, " ");
